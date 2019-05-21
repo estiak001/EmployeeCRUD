784e9fc395ad6f03be6ee17d43ab2b64e52e73c5
estiak001 <en.estiak@gmail.com>
2019-05-21 15:58:15 +0600

    first commit

---
+++ ASP.netCoreMVC_CRUD/Models/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.netCoreMVC_CRUD.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+
+        [Column(TypeName ="nvarchar(250)")]
+        [Required(ErrorMessage ="This field required")]
+        [DisplayName("Full Name")]
+        public string FullName { get; set; }
+
+
+        [Column(TypeName = "varchar(10)")]
+        [DisplayName("Employe Code")]
+        public string EmployeeCode { get; set; }
+
+        [DisplayName("Position")]
+        [Column(TypeName = "varchar(100)")]
+        public string Position { get; set; }
+
+        [DisplayName("Office Location")]
+
+        [Column(TypeName = "varchar(100)")]
+        public string OfficeLocation { get; set; }
+    }
+}
